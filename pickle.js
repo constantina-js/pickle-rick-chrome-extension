d0d720fec2dca151162409be04d211105552930f
conscou <scoullisconstantina@gmail.com>
2017-08-14 01:23:24 -0400

    modified comment format

--- pickle.js
+++ pickle.js
@@ -1,8 +1,8 @@
-var div = document.createElement("DIV");				//create div element
-div.id = "pickleRick";									//give div element id
+var div = document.createElement("DIV");	//create div element
+div.id = "pickleRick";		//give div element id
 
-var img = document.createElement("IMG");				//create img element
+var img = document.createElement("IMG");	//create img element
 img.src = chrome.runtime.getURL('img/front-pickle.png');	//get img src
 
-div.appendChild(img);									//add img to div
-document.body.appendChild(div);							//add div to body
+div.appendChild(img);	//add img to div
+document.body.appendChild(div);		//add div to body
