a117a104020c5e4793bfbea83926aee7cbe29c3f
conscou <scoullisconstantina@gmail.com>
2017-08-16 02:36:19 -0400

    you can flip Pickle Rick

--- pickle.js
+++ pickle.js
@@ -1,13 +1,28 @@
+
 //create div element and assign div id
 var div = document.createElement("DIV");	
 div.id = "pickleRick";		
 
 //create img element and provided img source
 var img = document.createElement("IMG");
-img.src = chrome.runtime.getURL('img/front-pickle.png');
+img.id = "pickleRickImg"
+img.src = chrome.runtime.getURL('img/back-pickle.png');
 
 //added img to div and then add div to body
 div.appendChild(img);
 document.body.appendChild(div);	
 
-console.log("Pickle RICKKKKK")
+//flip the pickle over, Morty
+img.addEventListener("click", function(){
+
+	if(img.src==chrome.runtime.getURL('img/back-pickle.png')){
+		document.getElementById("pickleRickImg").src = chrome.runtime.getURL('img/front-pickle.png');
+	} else {
+		document.getElementById("pickleRickImg").src = chrome.runtime.getURL('img/back-pickle.png');
+	}
+
+});
+
+
+
+
